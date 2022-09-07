90f47f508909759615df73f17e6372a16b778b6b
Jaewon <sorhd134@gmail.com>
2022-09-07 13:47:42 +0900

    Custom Admin

--- users/admin.py
+++ users/admin.py
@@ -7,4 +7,24 @@ from .models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (
+            "프로필",
+            {"fields": ("username", "password", "name", "email", "is_host")},
+        ),
+        (
+            "권한",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        ("날짜", {"fields": ("last_login", "date_joined"), "classes": ("collapse",)}),
+    )
+    list_display = ("username", "email", "name", "is_host")
