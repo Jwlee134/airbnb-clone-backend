142c9b9cc6a4d13971fa58ff1d59bc4e7c43c8d5
Jaewon <sorhd134@gmail.com>
2022-10-11 16:22:57 +0900

    APIView

--- categories/views.py
+++ categories/views.py
@@ -3,20 +3,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 from .models import Category
 from .serializers import CategorySerializer
 
 # Create your views here.
 
-# GET, POST /categories
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+
+class Categories(APIView):
+    def get(self, request):
         categories = Category.objects.all()
         # ORM을 통해 가져온 querySet을 CategorySerializer class에 정의된대로 JSON으로 변환
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         # 사용자가 보낸 JSON을 CategorySerializer class에 정의된대로 querySet으로 변환
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
@@ -27,21 +28,21 @@ def categories(request):
             return Response(serializer.errors)
 
 
-# GET /category/1
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
+class CategoryDetail(APIView):
+    # 각 메소드마다 공통으로 사용되는 객체를 리턴할 때 만드는 일종의 컨벤션 메소드
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
 
-    if request.method == "GET":
-        # try except는 블록 개념 없음
-        serializer = CategorySerializer(category)
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
-    elif request.method == "PUT":
+
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,  # 특정 필드만 업데이트를 위해 필수 필드도 옵셔널하게 만들고 싶을 때
         )
@@ -54,6 +55,7 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
-    elif request.method == "DELETE":
-        category.delete()
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)
