8d8ed461a6f259716591208d26f288799f82ad0c
Jaewon <sorhd134@gmail.com>
2022-12-26 15:30:28 +0900

    JWT Decode

--- config/authentication.py
+++ config/authentication.py
@@ -1,6 +1,8 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from users.models import User
+from django.conf import settings
+import jwt
 
 
 class TrustMeBroAuthentication(BaseAuthentication):
@@ -13,3 +15,19 @@ class TrustMeBroAuthentication(BaseAuthentication):
             return (user, None)
         except User.DoesNotExist:
             raise AuthenticationFailed(f"No user with username: {username}")
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get("Token")
+        if not token:
+            return None
+        decoded = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        pk = decoded["pk"]
+        if not pk:
+            raise AuthenticationFailed("Invalid token detected.")
+        try:
+            user = User.objects.get(pk=pk)
+            return (user, None)
+        except User.DoesNotExist:
+            raise AuthenticationFailed(f"No user with id: {pk}")
